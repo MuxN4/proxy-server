ffc9c5ce629d6d5e119532f8dce9f7ab5458b9e2
Steve <stev3npy@gmail.com>
2024-08-05 09:52:39 +0300

    Logging configurations to track the activities of the proxy server

--- logger.py
+++ logger.py
@@ -1,4 +1,25 @@
 import logging
+from logging.handlers import RotatingFileHandler
+import os
+from config import ENABLE_LOGGING, LOG_FILE,DEBUG
 
-def setup_logger():
-    pass
+def setup_logger(name = "proxy_server"):
+    logger = logging.getLogger(name)
+
+    logger.setLevel(logging.DEBUG if DEBUG else logging.info)
+
+    file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    console_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+    if ENABLE_LOGGING:
+        log_dir = os.path.dirname(LOG_FILE)
+        if log_dir and not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+
+        file_handler = RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
+        file_handler.setFormatter(file_formatter)
+        logger.addHandler(file_handler)
+
+    return logger
+
+logger = setup_logger()
