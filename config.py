ade2bb223d28ce5ce9df9aaf64490d5cb19b70e1
Steve <stev3npy@gmail.com>
2024-08-05 09:30:53 +0300

    Configurations for the proxy

---
+++ config.py
@@ -0,0 +1,14 @@
+HOST = 'localhost'
+PORT = 8080
+BUFFER_SIZE = 4096
+
+MAX_CONNECTIONS = 5
+
+# connection timeout in seconds
+CONNECTION_TIMEOUT = 30
+
+ENABLE_LOGGING = True
+
+LOG_FILE = 'proxy_server.log'
+
+DEBUG = True
