23100a92e2f622c6c97a16c661fcaf38eeb3aeeb
Steve <stev3npy@gmail.com>
2024-08-05 09:27:53 +0300

    Entry point for the server

---
+++ main.py
@@ -0,0 +1,15 @@
+from proxy_handler import ProxyHandler
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+class ProxyHandler:
+    def __init__(self):
+        pass
+
+    def handle_client_request(self):
+        pass
