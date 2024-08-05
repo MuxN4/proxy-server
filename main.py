28d111e0336ab52d0cf58e45e46ca0a7c9c16014
Steve <stev3npy@gmail.com>
2024-08-05 10:50:26 +0300

    Updated to ensure the server runs or executes

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from proxy_handler import ProxyHandler
+import sys
+from proxy_handler import run_proxy
+from config import HOST, PORT
+from logger import logger
 
 def main():
-    pass
+    try:
+        logger.info(f"Starting proxy server on {HOST} : {PORT}")
+        run_proxy(HOST, PORT)
+    except KeyboardInterrupt:
+        logger.info("Proxy server stopped by user")
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
